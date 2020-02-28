8dfde8427bb9b67286d1469d654accdd24c2a42d
Den <denysdiachenko1997@gmail.com>
2020-02-28 15:30:44 +0200

    Initial Commit

---
+++ Live-News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Live-News
+//
+//  Created by Ден on 28.02.2020.
+//  Copyright © 2020 Ден. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
